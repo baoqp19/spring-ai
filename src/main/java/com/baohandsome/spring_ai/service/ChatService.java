9ade31f5b990ea5254e516312074129cd71efc56
= <minhducdtr70@gmail.com>
2025-07-05 11:27:55 +0700

    prompt ai

--- src/main/java/com/baohandsome/spring_ai/service/ChatService.java
+++ src/main/java/com/baohandsome/spring_ai/service/ChatService.java
@@ -1,6 +1,9 @@
 package com.baohandsome.spring_ai.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
 
 import com.baohandsome.spring_ai.dto.ChatRequest;
@@ -15,8 +18,17 @@ public class ChatService {
     }
 
     public String chat(ChatRequest request) {
+
+        // """ khối văn bản nhiều dòng
+        SystemMessage systemMessage = new SystemMessage("""
+                you are phamquocbao
+                """);
+        UserMessage userMessage = new UserMessage(request.message());
+
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
         return chatClient
-                .prompt(request.message())
+                .prompt(prompt)
                 .call()
                 .content();
     }
